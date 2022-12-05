d983c1baba08e1dc62e7efddbf556aa6f327a76f
mitchelleokemwa <mitchelleokemwa@gmail.com>
2022-12-05 21:57:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,69 +1,68 @@
-//Your code here
-const baseUrl "http://localhost:3000/characters";
-
-//Fetch Character Names
-function fetchCharacterNames (){ 
-    return fetch(baseUrl)
-    .then(response => response.json())
-}
-
-function renderCharacterNames(character) {
-  const characterBar= document.getElementById("character-bar");
-  const span = document.createElement("span");
-  span.innerHTML = character.name;
-  characterBar.appendChild(span);
-  span.dataset.id = character.id;
-  span.addEventListener("click", onSpanCharacterClick);
-}
-
-fetchCharacterNames().then(characters => {
-  characters.forEach(character => {
-    renderCharacterNames(character);
-  })
+document.addEventListener("DOMContentLoaded", () => {
+    getChar()
+    // displayChar()
 })
-
-//Fetching character details
-function fetchCharacterDetails (id) {
-  return fetch(baseUrl + `/$(id)`)
-  .then(response => response.json())
+let url = "http://localhost:3000/characters"
+function getChar(){
+    fetch(url)
+    .then(res => res.json())
+    .then(info => {
+        console.log(info)
+        info.forEach(characters => {
+        // console.log(characters);
+        displayChars(characters);
+    });
+})
 }
-
-function onSpaceCharacterClick (event) {
-  fetchCharacterDetails (event.target.dataset.id) 
-    .then(renderCharacterDetails);
-  }
-
-function renderCharacterDetails(character) {
-  const characterInfo = document.getElementById("detailed-info");
-  const charName = document.getElementById("name");
-  charName.innerText = character.name
-
-  const charImg = document.getElementById("image");
-  charImg.scr = character.name
-
-  const charVotes = document.getElementById("vote-count");
-  charVotes.innerText = character.votes
+function displayChars(characters){
+    // characters.preventDefault()
+    console.log(characters.name);
+    let name = document.createElement('p')
+    name.textContent = characters.name
+    let main = document.getElementById("main")
+    main.append(name)
+    name.addEventListener('click', () =>{
+        getOneChar(characters)
+    })
 }
-   
-//FRom Submission and Updating VOtes
-document.getElementById("votes-form").addEventListener("submit", (event)  => {
-  event.preventDefault();
-  const votesForm = event.target;
-  const votes = document.getElementById("vote-count")
-  votes.innerText = parseInt(votesForm.voes.value) + parseInt(votes.innerText)
-  votesForm.rest();
-})
-
-//Reset Button Functionality
-document.getElementById("reset-btn").addEventListener("click", () {
-  document.getElementById("vote-count").innerText = 0;
-})
-
-document.addEventListener("DOMContentLOaded", function () {
-  fetchCharacterNames();
-  fetchCharacterDetails();
-})
-         
-     
-   
-   
+function getOneChar(characters){
+    document.getElementById("charname").textContent = characters.name
+    document.getElementById("charimg").src = characters.image
+    document.getElementById("charvotes").textContent = `Votes cast: ${characters.votes}`
+    let btn = document.getElementById("charvotes")
+    btn.textContent = `Votes: ${characters.votes}`
+    btn.addEventListener('click', () => {
+        characters.votes+=1
+        btn.textContent = `Votes: ${characters.votes}`
+    })
+        let btn2 = document.getElementById("reset")
+        // btn2.textContent = `Votes: ${characters.votes}`
+        btn2.addEventListener('click', () => {
+            characters.votes = 0
+            btn.textContent = `Votes: ${characters.votes}`
+            resetVotes(characters)
+            // btn.textContent = `Votes: ${characters.votes}`
+    })
+}
+// function updateVotes(characters){
+//     fetch(`http://localhost:3000/characters/${characters.id}`, {
+//         method:'PATCH',
+//         headers:{
+//             'Content-type': 'application/json'
+//         },
+//         body: JSON.stringify(characters)
+//     })
+//     .then(res => res.json())
+//     .then(characters => console.log(characters))
+// }
+function resetVotes(characters){
+    fetch(`http://localhost:3000/characters/${characters.id}`, {
+        method:'PATCH',
+        headers:{
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(characters)
+    })
+    .then(res => res.json())
+    .then(characters => console.log(characters))
+}
